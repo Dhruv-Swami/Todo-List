23f63cbd01853663c64247497fc6481f8daed6c9
Dhruv-Swami <dhruvswaminhr@gmail.com>
2024-05-25 18:44:42 +0530

    updated

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -17,12 +17,12 @@ export const Edit = ({ editTodo, task }) => {
       <input
         type="text"
         className="outline-none bg transparent border border-gray-500 p-4 w-[300px]
-        text-black mb-8 rounded placeholder:text-gray-300"
+        text-black mb-1 ml-6 rounded placeholder:text-gray-300"
         placeholder="update task"
         onChange={(e) => setValue(e.target.value)}
         value={value}
       />
-      <button className="bg-gray-700 border-none p-2 text-white cursor-pointer rounded ml-2">
+      <button className="bg-red-700 border-none p-2 text-white cursor-pointer rounded ml-2  ">
         Update Task
       </button>
     </form>
--- src/components/Form.js
+++ src/components/Form.js
@@ -22,7 +22,7 @@ export const Form = ({ createTodo }) => {
         onChange={(e) => setValue(e.target.value)}
         value={value}
       />
-      <button className="bg-gray-700 border-none p-2 text-white cursor-pointer rounded ml-2">
+      <button className="bg-red-700 border-none p-2 text-white cursor-pointer rounded ml-2">
         Add Task
       </button>
     </form>
